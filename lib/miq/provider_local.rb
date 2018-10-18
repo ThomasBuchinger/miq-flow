bcd0ae0e4fc1addaf0cb64b5f435d88f84f078e3
ThomasBuchinger <ThomasBuchinger@users.noreply.github.com>
2018-10-18 12:27:17 +0200

    add path to Rakefile

--- lib/miq/provider_local.rb
+++ lib/miq/provider_local.rb
@@ -10,7 +10,7 @@ module GitFlow
 
       def import(tmpdir, fs_domain, miq_domain)
         commands =  [
-          "rake evm:automate:import DOMAIN=#{fs_domain} IMPORT_AS=#{miq_domain} IMPORT_DIR=#{tmpdir}/automate OVERWRITE=true PREVIEW=false ENABLED=true"
+          "rake -f /var/www/miq/vmdb/Rakefile evm:automate:import DOMAIN=#{fs_domain} IMPORT_AS=#{miq_domain} IMPORT_DIR=#{tmpdir}/automate OVERWRITE=true PREVIEW=false ENABLED=true"
         ]
         $logger.debug('Importing with Appliance provider')
         commands.each do |cmd|
