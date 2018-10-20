efffbcdfdc65a12e34506a4df5a797e17dc8082f
Thomas Buchinger <thomasbuchinger@hotmail.com>
2018-10-20 11:26:29 +0000

    uses merge_base in diff instead of master. And comments

--- lib/feature.rb
+++ lib/feature.rb
@@ -2,6 +2,9 @@ require_relative 'git/mixin_git.rb'
 require_relative 'miq/mixin_miq.rb'
 
 module GitFlow
+  # A feature the top level object representing a feature-branch.
+  # It includes aspects of handling git branches as well as ManageIQ Automate domains
+  #
   class Feature
     include GitFlow::GitMethods
     include GitFlow::MiqMethods
@@ -10,24 +13,25 @@ module GitFlow
     attr_accessor :miq_domain
     attr_reader   :git_repo
 
+    # Sets up a bunch of instance variables 
+    #
+    # @option opts [String] :remote_name('origin')
+    # @option opts [String] :base('master')
+    # @option opts [Array<String>]        :prefix(feature, fix)
+    # @option opts [GitFlow::MiqProvider] :provider(GitFlow::MiqProvider::Noop)
+    # @option opts [String] :automate_dir('automate')
+    # @option opts [String] :miq_priority(10)
+    # @option opts [String] :miq_fs_domain(nil)
     def _set_defaults(opts={})
-      @remote_name   = opts.fetch(:remote_name, 'origin')
-      @master        = opts.fetch(:master, 'master')
-      @prefixes      = opts.fetch(:prefix, ['feature', 'fix'] )
-      @miq_provider  = opts.fetch(:provider, GitFlow::MiqProvider::Noop.new )
-      @automate_dir  = opts.fetch(:automate_dir, 'automate' )
-      @miq_prioritiy = opts.fetch(:miq_priority, 'automate' )
+      @remote_name   = opts.fetch(:remote_name,   'origin')
+      @base          = opts.fetch(:base,          'master')
+      @prefixes      = opts.fetch(:prefix,        ['feature', 'fix'] )
+      @miq_provider  = opts.fetch(:provider,      GitFlow::MiqProvider::Noop.new )
+      @automate_dir  = opts.fetch(:automate_dir,  'automate' )
+      @miq_prioritiy = opts.fetch(:miq_priority,  10 )
       @miq_fs_domain = opts.fetch(:miq_fs_domain, nil )
     end
 
-    def _create_git(branch_name, repo)
-
-      @git_master = repo.branches[@master]
-      @git_branch = repo.branches["#{@remote_name}/#{branch_name}"]
-
-      raise "Unable to find base branch: #{@master}"        if @git_master.nil?()
-      raise "Unable to find feature branch: #{branch_name}" if @git_branch.nil?()
-    end
 
     def _create_miq(domain_name)
       @miq_domain    = domain_name
@@ -35,7 +39,11 @@ module GitFlow
       @miq_import_method = :dirty
     end
     
-    def initialize(branch_name, opts) 
+    # Represents a feature-branch
+    #
+    # @param [String] branch_name 
+    # @option opts @see _set_defaults
+    def initialize(branch_name, opts={}) 
       miq_domain = opts.fetch(:miq_domain, branch_name.split(/-/)[2]) || branch_name
       @git_repo  = opts.fetch(:git_repo, nil) || $git_repo
       _set_defaults(opts)
@@ -46,12 +54,14 @@ module GitFlow
       _create_miq(miq_domain)
     end
 
+    # Deploys the feature to ManageIQ
+    #
     def deploy()
       @git_repo.checkout(@git_branch)
       $logger.debug("Deploying: #{@miq_domain}") 
 
       tmpdir = prepare_import(@miq_import_method, @miq_domain)
-      @miq_provider.import(tmpdir, @miq_fs_domain, @miq_domain)
+      @miq_provider.imuport(tmpdir, @miq_fs_domain, @miq_domain)
       cleanup_import(@miq_import_method)
       
     end
--- lib/git/mixin_git.rb
+++ lib/git/mixin_git.rb
@@ -1,37 +1,76 @@
 require 'rugged'
 
 module GitFlow
+  # The module where all the git related stuff goes in
   module GitMethods
-  
-    def self._sync_branches(repo)
-      remotes = repo.branches.each(:remote).select{|remote_branch| @@prefixes.any?{|prefix| remote_branch.name.match("#{@@remote_name}/#{prefix}")  } }
-      $logger.debug("_sync_branches: Found Remotes #{remotes.map{|r| r.name}}")
-      locals = repo.branches.each_name(:local).sort
 
-      branch_diff = remotes.map do |r|
-        next nil if locals.any?{|local_branch_name| r.name == local_branch_name  }
+    # This sets up the git half of a feature
+    # 
+    # @git_base: The baseline Rugged::Commit to diff against
+    # @git_branch: Rugged::Branch of origin/<branch> 
+    #
+    # @param [String] branch_name The branch-name as string
+    # @param [Rugged::Repository] repo
+    def _create_git(branch_name, repo)
+      rbranch = "#{@remote_name}/#{branch_name}"
+      @git_base   = merge_base(repo, rbranch)
+      @git_branch = repo.branches[rbranch]
 
-        $logger.debug("Creating Branch: #{r.name}")
-        
-      end
-      branch_diff.compact()
+      raise "Unable to find base branch: #{@base}"        if @git_base.nil?()
+      raise "Unable to find feature branch: #{branch_name}" if @git_branch.nil?()
     end
 
-    def get_diff_paths(base=nil, branch=nil)
-      base   = base   || @git_master
-      branch = branch || @git_branch
+    # Find the merge_base commit
+    # https://git-scm.com/docs/git-merge-base
+    #
+    # @param [Rugged::Repository] repo 
+    # @param [String] oid1 
+    # @param [String] oid2 defaults to @base => 'master'
+    # @return [Rugged::Commit, nil] 
+    def merge_base(repo, oid1, oid2=nil)
+     oid2 = oid2 || @base || 'master'
+
+     base = repo.merge_base(oid1, oid2)
+     base.nil?() ? nil : repo.lookup(base)
+    end
+  
+    # Get a list of changed files 
+    #
+    # @param [Rugged::Commit, Rugged::Branch] base defaults to @git_base
+    # @param [Rugged::Commit, Rugged::Branch] base defaults to @git_branch
+    # @return [Array<String>] A list of changed paths
+    def get_diff_paths(base=nil, head=nil)
+      base = base || @git_base
+      head = head || @git_branch
 
       raise "Diff failed: base is nil"   if base.nil?
-      raise "Diff failed: branch is nil" if branch.nil?
+      raise "Diff failed: branch is nil" if head.nil?
 
       paths = []
-      diff = base.target.diff(branch.target)
+      base_commit = base.kind_of?(Rugged::Reference) ? base.target : base
+      head_commit = head.kind_of?(Rugged::Reference) ? head.target : head
+
+      diff = base_commit.diff(head_commit)
       diff.each_delta do |delta|
         next if delta.deleted?()
         paths << delta.new_file[:path]
       end
       paths
     end
+
+#    def self._sync_branches(repo)
+#      remotes = repo.branches.each(:remote).select{|remote_branch| @@prefixes.any?{|prefix| remote_branch.name.match("#{@@remote_name}/#{prefix}")  } }
+#      $logger.debug("_sync_branches: Found Remotes #{remotes.map{|r| r.name}}")
+#      locals = repo.branches.each_name(:local).sort
+#
+#      branch_diff = remotes.map do |r|
+#        next nil if locals.any?{|local_branch_name| r.name == local_branch_name  }
+#
+#        $logger.debug("Creating Branch: #{r.name}")
+#        
+#      end
+#      branch_diff.compact()
+#    end
 
   end
 end
