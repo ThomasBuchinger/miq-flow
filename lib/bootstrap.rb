983a827c7d8760e86ab4b85a6d0737e78e472bc5
Thomas Buchinger <thomasbuchinger@hotmail.com>
2018-10-24 01:10:12 +0000

    fix #7: move GitFlow module to seperate file

--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -2,8 +2,10 @@ require 'rugged'
 require 'tmpdir'
 require 'logger'
 
-$LOAD_PATH << File.dirname(__FILE__)
+lib_dir = __dir__
+$LOAD_PATH << lib_dir
 require 'feature.rb'
+require 'gitflow.rb'
 require 'miq/provider_docker.rb'
 require 'miq/provider_local.rb'
 require 'miq/provider_noop.rb'
@@ -12,64 +14,6 @@ $default_opts = { :clear_tmp => true }
 $default_opts[:feature_defaults] = {:miq_fs_domain=>'foo', :miq_provider=>GitFlow::MiqProvider::Noop.new()}
 $default_opts[:git_opts] = {}
 GitFlow.process_environment_variables()
-require_relative '../custom.rb' if File.file?('../custom.rb')
+require_relative '../custom.rb' if File.file?(File.expand_path(File.join(lib_dir, '..', 'custom.rb')))
 $default_opts.freeze()
 GitFlow.validate()
-
-
-module GitFlow
-  def self.init()
-    # prepare directories
-    #
-    $tmpdir = Dir.mktmpdir('miq_import_')
-    Dir.mkdir(File.join($tmpdir, 'repo'))
-    Dir.mkdir(File.join($tmpdir, 'import'))
-
-    # configure logging
-    #
-    $logger  = Logger.new(STDOUT)
-    $logger.level = $default_opts.fetch(:log_level, Logger::INFO)
-
-    # get git repository
-    #
-    prepare_repo($default_opts[:git_opts])
-  end
-  def self.tear_down()
-    FileUtils::rm_rf($tmpdir)
-  end
-  def self.validate()
-    raise "No git repository specified" if $git_url.nil? and $git_path.nil?
-  end
-
-  def self.process_environment_variables()
-    $git_url      = ENV['GIT_URL']
-    $git_user     = ENV['GIT_USER']
-    $git_password = ENV['GIT_PASSWORD']
-    $git_path     = ENV['GIT_PATH']
-
-    $default_opts[:log_level] = Logger::DEBUG if ENV['VERBOSE'] == 'true'
-    $default_opts[:log_level] = Logger::WARN  if ENV['QUIET'] == 'true'
-
-    $default_opts[:clear_tmp] = false  if ENV['CLEAR_TMP'] == 'false'
-  end
-
-  def self.prepare_repo(opts)
-    clone_repo(opts) unless $git_url.nil?
-    local_repo(opts) unless $git_path.nil?
-  end
-  def self.clone_repo(opts)
-    $logger.info("Cloning git Repository from: #{$git_url}")
-    dir = File.join($tmpdir, 'repo')
-
-    # make Credentials
-    opts[:credentials] = Rugged::Credentials::UserPassword.new(username: $git_user, password: $git_pssword) if $git_user and $git_password
-
-    $git_repo = Rugged::Repository.clone_at($git_url, dir, opts)
-    raise "Failed to clone repository at #{$git_url}" if $git_repo.nil?
-  end
-  def self.local_repo(opts)
-    $logger.info("Using git Repository: #{$git_path}")
-    $git_repo = Rugged::Repository.discover($git_path, opts.fetch(:accross_fs, true))
-    raise "Failed to clone repository at #{$git_path}" if $git_repo.nil?
-  end
-end
---
+++ lib/gitflow.rb
@@ -0,0 +1,56 @@
+module GitFlow
+  def self.init()
+    # prepare directories
+    #
+    $tmpdir = Dir.mktmpdir('miq_import_')
+    Dir.mkdir(File.join($tmpdir, 'repo'))
+    Dir.mkdir(File.join($tmpdir, 'import'))
+
+    # configure logging
+    #
+    $logger  = Logger.new(STDOUT)
+    $logger.level = $default_opts.fetch(:log_level, Logger::INFO)
+
+    # get git repository
+    #
+    prepare_repo($default_opts[:git_opts])
+  end
+  def self.tear_down()
+    FileUtils::rm_rf($tmpdir)
+  end
+  def self.validate()
+    raise "No git repository specified" if $git_url.nil? and $git_path.nil?
+  end
+
+  def self.process_environment_variables()
+    $git_url      = ENV['GIT_URL']
+    $git_user     = ENV['GIT_USER']
+    $git_password = ENV['GIT_PASSWORD']
+    $git_path     = ENV['GIT_PATH']
+
+    $default_opts[:log_level] = Logger::DEBUG if ENV['VERBOSE'] == 'true'
+    $default_opts[:log_level] = Logger::WARN  if ENV['QUIET'] == 'true'
+
+    $default_opts[:clear_tmp] = false  if ENV['CLEAR_TMP'] == 'false'
+  end
+
+  def self.prepare_repo(opts)
+    clone_repo(opts) unless $git_url.nil?
+    local_repo(opts) unless $git_path.nil?
+  end
+  def self.clone_repo(opts)
+    $logger.info("Cloning git Repository from: #{$git_url}")
+    dir = File.join($tmpdir, 'repo')
+
+    # make Credentials
+    opts[:credentials] = Rugged::Credentials::UserPassword.new(username: $git_user, password: $git_pssword) if $git_user and $git_password
+
+    $git_repo = Rugged::Repository.clone_at($git_url, dir, opts)
+    raise "Failed to clone repository at #{$git_url}" if $git_repo.nil?
+  end
+  def self.local_repo(opts)
+    $logger.info("Using git Repository: #{$git_path}")
+    $git_repo = Rugged::Repository.discover($git_path, opts.fetch(:accross_fs, true))
+    raise "Failed to clone repository at #{$git_path}" if $git_repo.nil?
+  end
+end
