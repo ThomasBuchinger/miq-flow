0bb4030280004a0eb5f224304e9dad9dee286c47
ThomasBuchinger <ThomasBuchinger@users.noreply.github.com>
2018-10-31 19:08:44 +0100

    rewording parameter description

--- example_custom.rb
+++ example_custom.rb
@@ -1,16 +1,15 @@
 # Set logging level
 # $default_opts[:log_level] = Logger::DEBUG
 
-# Configure remote git repository
+# Configure git repository
 #
-# Git URL:
-# Configure a remote repository, is cloned to a temporary directory when needed.
+# Configure a remote repository (which will be cloned to a temporary directory when needed)
 # On ManageIQ appliances, only HTTPS URLs are allowed, because SSH support must be enabled in rugged at complile time
 # $git_url  = 'https://github.com/ThomasBuchinger/automate-example.git'
 # Authentication for remote repositories
 # $git_user = someone
 # $git_password = TOKEN
-# Configure a local git repository. 
+# Configure a local git repository (relative to the current working direcory). 
 # The local repository is not updated automatically
 # WARNING: The script may change the checked out branch and create new branches
 # $git_path     = 'path/to/your/automate-repository'
