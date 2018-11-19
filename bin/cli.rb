5dacd76123b9d3cf7411c252c643283304417570
Thomas Buchinger <thomasbuchinger@hotmail.com>
2018-11-19 01:05:30 +0000

    move cli to the new implementation

--- bin/cli.rb
+++ bin/cli.rb
@@ -40,21 +40,19 @@ module GitFlow
     desc 'devel1', 'Development NOOP command' 
     def devel1()
       GitFlow.init()
-      feature_opts = { :miq_fs_domain=>'buc', :miq_domain=>'f1' }
-      f1 = GitFlow::Feature.new('feature-1-f1', $default_opts[:feature_defaults].merge(feature_opts))
-      f1.discover_domains()
-#      f1.deploy()
+      feature_opts = { :feature_name=>'f1' }
+      f1 = GitFlow::Feature.new('feature-1-f1', feature_opts)
+      f1.deploy()
       GitFlow.tear_down()
     end
     desc 'devel2', 'Development ACTIVE command ' 
     def devel2()
       GitFlow.init()
-      provider = GitFlow::MiqProvider::Docker.new()
-      feature_opts = { :miq_fs_domain=>'buc', :miq_domain=>'base', :provider=>provider, :miq_import_method=>:clean, :automate_dir=>'automate'}
-      master = GitFlow::Feature.new('master', $default_opts[:feature_defaults].merge(feature_opts))
+      feature_opts = { :feature_name=>'base', :provider=>'docker', :miq_import_method=>:clean}
+      master = GitFlow::Feature.new('master', feature_opts)
       master.deploy()
-      feature_opts = { :miq_fs_domain=>'buc', :miq_domain=>'f1', :provider=>provider }
-      f1 = GitFlow::Feature.new('feature-1-f1', $default_opts[:feature_defaults].merge(feature_opts))
+      feature_opts = { :feature_name=>'f1', :provider=>'docker' }
+      f1 = GitFlow::Feature.new('feature-1-f1', feature_opts)
       f1.deploy()
       GitFlow.tear_down()
     end
