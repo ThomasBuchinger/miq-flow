ThomasBuchinger/miq-flow

commit 9189ddeb69c10089b7e4832acf0241e31fb91f94

Support multiple Ae Domains and discover Domain params  (#17)
Before this commit, the Feature class was representing a git feature branch AND a ManageIQ Automate domain. limiting the implementation to only 1 Automate domain per feature branch

This commit breaks the domain related code into its own class and adds a discovery mechanism for domain parameters, which were previously derived from a mixture of configuration and good guessing.
* Add support for multiple domains
* Add discovery for domains (replacing configuration/convention)
* Add list command
* Add inspect command
* Minor cleanup and docs and stuff

lib/domain.rb
@@ -1,0 +1,109 @@
+module GitFlow
+  class MiqDomain
+    include GitFlow::MiqMethods
+    # Mandatory parameters
+    attr_accessor :name
+    # Mandatory parameters with guessable defaults
+    attr_accessor :miq_provider, :import_method, :export_dir, :export_name
+
+    # Optional Parameters
+    attr_accessor :miq_priority
+    attr_reader :changeset, :branch_name
+
+    # Sets up a bunch of instance variables 
+    #
+    # @option opts [String] :miq_provider(noop) CHOICE: noop, local, docker
+    # @option opts [String] :export_dir relative path to the domain export from git working dir
+    # @option opts [String] :miq_import_method(partial) CHOICE partial, clean
+    #                       partial: imports only changed files
+    #                       clean: imports everything
+    # @option opts [String] :miq_priority DOES NOTHING, since the importer does not honor it
+    # @option opts [String] :branch_name name of the git branch. INFO only
+    def _set_defaults(opts={})
+      @miq_provider_name = opts.fetch(:miq_provider,      'noop')
+      @export_dir        = opts.fetch(:export_dir,        'automate' )
+      @export_name       = opts.fetch(:export_name,       @name )
+      @miq_import_method = opts.fetch(:miq_import_method, :partial)
+      @miq_priority      = opts.fetch(:miq_priority,      10 )
+      @branch_name       = opts.fetch(:branch_name,       'No Branch' )
+    end
+
+    # Filter changed files in this Automate domain from the list of all files
+    #
+    def _limit_changeset(files)
+      @changeset = files.select {|f| f.include?(@export_name)}
+    end
+
+    # create a new MiqDomain Object from information on the file system
+    # @see #find_domain_files
+    #
+    # @param [Hash] dom domain information
+    # @return [GitFlow::MiqDomain] new MiqDomain object
+    def self.create_from_file(dom)
+      opts = {}
+      opts[:export_name]   = dom[:domain_name]
+      opts[:export_dir]    = dom[:relative_path]
+      opts[:import_method] = dom[:import_method] 
+      opts[:provider_name] = dom[:provider]
+      opts[:branch_name]   = dom[:branch_name]
+      
+      new_name  = "feature_#{dom[:feature_name]}_#{opts[:export_name]}"
+      opts.select!{|_, value| !value.nil? }
+      self.new(new_name, opts)
+    end
+    
+    # NOT IN USE
+    #
+    def self.create_from_config(name, opts)
+      self.new(name, opts)
+    end
+
+    # Represents a Auromate Domain
+    #
+    # @param [String] name for the imported domain
+    # @option opts @see _set_defaults
+    # @option opts [String] :provider_name CHOICE: noop, local, docker
+    def initialize(name, opts)
+      @name = name
+      _set_defaults(opts)
+
+      @miq_provider = GitFlow::MiqProvider::Noop.new      if opts[:provider_name] == 'noop'
+      @miq_provider = GitFlow::MiqProvider::Appliance.new if opts[:provider_name] == 'local'
+      @miq_provider = GitFlow::MiqProvider::Docker.new    if opts[:provider_name] == 'docker'
+      @miq_provider = GitFlow::MiqProvider::Noop.new      if @miq_provider.nil?
+    end
+
+    def prepare_import(domain_data, feature_data)
+      begin 
+        self.send("prepare_import_#{@miq_import_method}".to_sym, domain_data, feature_data)
+      rescue NoMethodError => err
+        return {:error=>true, :miq_import_method=>@miq_import_method}
+      end
+    end
+    def cleanup_import(prep_data)
+      begin 
+        self.send("cleanup_import_#{@miq_import_method}".to_sym)
+      rescue NoMethodError => err
+        return {:error=>true, :miq_import_method=>@miq_import_method}
+      end
+    end
+
+    # Deploys (aka import) Automate Domains to ManageIQ 
+    #
+    # @option opts [Array<String>] :changeset changed files according to git
+    # @option opts [Boolean] :skip_emtpy do not create an empty domain if changeset is empty
+    def deploy(opts)
+      opts[:changeset] = _limit_changeset(opts.fetch(:changeset, []))
+      if opts[:skip_empty] and opts[:changeset].empty?()
+        $logger.info("Skipping Domain: #{@name}: empty")
+        return true
+      end
+      prep_data = prepare_import(self, opts)
+      raise GitFlow::Error, "Unknown Import method: #{prep_data[:miq_import_method]}" if prep_data[:error] == true
+      @miq_provider.import(File.join(prep_data[:import_dir], @export_dir), @export_name, @name)
+      clean_data = cleanup_import(prep_data)
+      raise GitFlow::Error, "Error calling cleanup method: #{prep_data[:miq_import_method]}" if clean_data[:error] == true
+    end
+
+  end
+end

lib/miq/method_partial.rb
@@ -1,0 +1,61 @@
+require 'tmpdir'
+require 'yaml'
+module GitFlow
+  # This module contains everything needed to prepare an Automate domain for import
+  # Mostly file handling at this point
+  module MiqMethods
+
+    def prepare_import_partial(dom, feat)
+      base_dir = "#{feat[:git_workdir]}"
+      import_dir = File.join($tmpdir, 'import')
+
+      paths            = feat[:changeset]
+      method_data      = paths.map{|p| p.gsub(/rb$/, 'yaml') }
+      $logger.debug("Doing a partial import of #{paths.join(', ')}")
+      all_files        = paths + method_data + Partial.find_parent_files(base_dir, paths)
+        
+      fake_domain_file = Partial.create_fake_domain(File.join(import_dir, dom.export_dir), dom.export_name, dom.branch_name, dom.miq_priority)
+      Partial.copy_to_tmp(import_dir, base_dir, all_files)
+      { :import_dir => import_dir }
+    end
+    def cleanup_import_partial()
+    end
+
+    module Partial
+
+      def self.find_parent_files(base_dir, paths)
+        parent_files = Dir.glob("#{base_dir}**/*").map{|f| f.gsub(base_dir, '') }.select() do |file|
+          keep = true
+          keep &= ['__namespace__.yaml', '__class__.yaml'].include?(File.basename(file))
+          keep &= paths.any?{ |p| p.start_with?(File.dirname(file)) }
+          keep 
+        end
+        parent_files
+      end
+      def self.create_fake_domain(import_dir, domain_name, branch_name, priority)
+        domain = {'object_type'=>'domain', 'version'=>1.0, 'object'=>{'attributes'=>{}}}
+        domain['object']['attributes']['name']         = domain_name
+        domain['object']['attributes']['description']  = "Development Branch for feature #{domain_name}: #{branch_name}"
+        domain['object']['attributes']['display_name'] = nil
+        domain['object']['attributes']['priority']     = priority
+        domain['object']['attributes']['enabled']      = true
+        domain['object']['attributes']['tenant_id']    = 1 
+        domain['object']['attributes']['source']       = 'user'
+        domain['object']['attributes']['top_level_namespace'] = nil 
+        filename = File.join(import_dir, domain_name, '__domain__.yaml')
+        $logger.debug("Creating Fake domain at #{filename}")
+
+        FileUtils.mkdir_p(File.dirname(filename))
+        File.write(filename, domain.to_yaml())
+      end
+      def self.copy_to_tmp(import_dir, base_dir, all_files)
+        $logger.debug("Copy to #{import_dir} Files: #{all_files}")
+        all_files.each do |file|
+          FileUtils.mkdir_p(File.join(import_dir, File.dirname(file)))
+          FileUtils.cp(File.expand_path(file, base_dir), File.join(import_dir, file), :preserve=>true)
+        end
+        import_dir
+      end
+    end
+  end
+end

bin/cli.rb
@@ -7,7 +7,17 @@ module GitFlow
 
     desc "list", "List Feature branches "
     def list()
+      GitFlow.init()
+      branches = GitFlow::GitMethods.get_remote_branches()
+      puts branches.map{|b| GitFlow::Feature.new(b.name, {}).show_summary() }
+    end
 
+    desc "inspect NAME", "List domains"
+    option :short, type: :boolean, default: false
+    def inspect(name)
+      GitFlow.init()
+      feature = GitFlow::Feature.new(name, {})
+      puts options[:short] ? feature.show_summary() : feature.show_details()
     end
 
     desc "deploy NAME", "Deploy a Feature Branch"
@@ -17,16 +27,12 @@ module GitFlow
     option :provider, desc: "How to talk to ManageIQ (default: noop)"
     def deploy(name)
       GitFlow.init()
-      miq_domain   = options[:domain] || (name.split(/-/) || [])[2] || name
-      provider     = GitFlow::MiqProvider::Appliance.new() if options[:provider] == 'local'
-      provider     = GitFlow::MiqProvider::Docker.new()    if options[:provider] == 'docker'
-      provider     = GitFlow::MiqProvider::Noop.new()      if options[:provider] == 'noop'
+      miq_domain = options[:domain] || (name.split(/-/) || [])[2] || name
+      provider   = options.fetch(:provider, 'default')
 
-      feature_opts = {:miq_domain=>miq_domain, :provider=>provider}
-      feature_opts[:miq_priority]  = options[:priority]    unless options[:priority].nil?
-      feature_opts[:provider]      = provider              unless provider.nil?
-      feature_opts[:miq_fs_domain] = options[:export_name] unless options[:export_name].nil?
-      feature = GitFlow::Feature.new(name, $default_opts[:feature_defaults].merge(feature_opts))
+      opts = {:feature_name=>miq_domain, :provider=>provider}
+      opts[:miq_priority]  = options[:priority]    unless options[:priority].nil?
+      feature = GitFlow::Feature.new(name, opts)
       feature.deploy()
       GitFlow.tear_down()
     end
@@ -34,20 +40,19 @@ module GitFlow
     desc 'devel1', 'Development NOOP command' 
     def devel1()
       GitFlow.init()
-      feature_opts = { :miq_fs_domain=>'buc', :miq_domain=>'f1' }
-      f1 = GitFlow::Feature.new('feature-1-f1', $default_opts[:feature_defaults].merge(feature_opts))
+      feature_opts = { :feature_name=>'f1' }
+      f1 = GitFlow::Feature.new('feature-1-f1', feature_opts)
       f1.deploy()
       GitFlow.tear_down()
     end
     desc 'devel2', 'Development ACTIVE command ' 
     def devel2()
       GitFlow.init()
-      provider = GitFlow::MiqProvider::Docker.new()
-      feature_opts = { :miq_fs_domain=>'buc', :miq_domain=>'base', :provider=>provider, :miq_import_method=>:clean, :automate_dir=>'automate'}
-      master = GitFlow::Feature.new('master', $default_opts[:feature_defaults].merge(feature_opts))
+      feature_opts = { :feature_name=>'base', :provider=>'docker', :miq_import_method=>:clean}
+      master = GitFlow::Feature.new('master', feature_opts)
       master.deploy()
-      feature_opts = { :miq_fs_domain=>'buc', :miq_domain=>'f1', :provider=>provider }
-      f1 = GitFlow::Feature.new('feature-1-f1', $default_opts[:feature_defaults].merge(feature_opts))
+      feature_opts = { :feature_name=>'f1', :provider=>'docker' }
+      f1 = GitFlow::Feature.new('feature-1-f1', feature_opts)
       f1.deploy()
       GitFlow.tear_down()
     end

example_custom.rb
@@ -1,6 +1,9 @@
 # Set logging level
 # $default_opts[:log_level] = Logger::DEBUG
 
+# Remove the temporary working directory on exit
+# $default_opts[:clear_tmp] = true
+
 # Configure git repository
 #
 # Configure a remote repository (which will be cloned to a temporary directory when needed)

lib/bootstrap.rb
@@ -4,14 +4,18 @@ require 'logger'
 
 lib_dir = __dir__
 $LOAD_PATH << lib_dir
-require 'feature.rb'
-require 'gitflow.rb'
 require 'miq/provider_docker.rb'
 require 'miq/provider_local.rb'
 require 'miq/provider_noop.rb'
+require 'miq/method_partial.rb'
+require 'miq/method_clean.rb'
+require 'miq/mixin_miq.rb'
+require 'domain.rb'
+require 'feature.rb'
+require 'gitflow.rb'
 
 $default_opts = { :clear_tmp => true }
-$default_opts[:feature_defaults] = {}
+$default_opts[:miq] = {}
 $default_opts[:git_opts] = {}
 require_relative '../custom.rb' if File.file?(File.expand_path(File.join(lib_dir, '..', 'custom.rb')))
 GitFlow.process_environment_variables()

lib/feature.rb
@@ -1,5 +1,6 @@
+require 'pathname'
 require_relative 'git/mixin_git.rb'
-require_relative 'miq/mixin_miq.rb'
+require_relative 'domain.rb'
 
 module GitFlow
   # A feature the top level object representing a feature-branch.
@@ -7,7 +8,7 @@ module GitFlow
   #
   class Feature
     include GitFlow::GitMethods
-    include GitFlow::MiqMethods
+    include GitFlow::MiqMethods::MiqUtils
 
     attr_accessor :git_branch, :git_master
     attr_accessor :miq_domain
@@ -18,55 +19,75 @@ module GitFlow
     # @option opts [String] :remote_name('origin')
     # @option opts [String] :base('master')
     # @option opts [Array<String>]        :prefix(feature, fix)
-    # @option opts [GitFlow::MiqProvider] :provider(GitFlow::MiqProvider::Noop)
-    # @option opts [String] :automate_dir('automate')
-    # @option opts [String] :miq_priority(10)
-    # @option opts [String] :miq_fs_domain(nil)
     def _set_defaults(opts={})
       @remote_name       = opts.fetch(:remote_name,       'origin')
       @base              = opts.fetch(:base,              'master')
       @prefixes          = opts.fetch(:prefix,            ['feature', 'fix'] )
-      @miq_provider      = opts.fetch(:provider,          nil )
-      @automate_dir      = opts.fetch(:automate_dir,      'automate' )
-      @miq_prioritiy     = opts.fetch(:miq_priority,      10 )
-      @miq_fs_domain     = opts.fetch(:miq_fs_domain,     nil )
-      @miq_import_method = opts.fetch(:miq_import_method, :dirty)
     end
 
 
-    def _create_miq(domain_name)
-      @miq_domain    = domain_name
-      @miq_fs_domain = @miq_fs_domain || domain_name
-      @miq_provider  = @miq_provider || GitFlow::MiqProvider::Noop.new
-    end
     
     # Represents a feature-branch
     #
     # @param [String] branch_name 
     # @option opts @see _set_defaults
-    def initialize(branch_name, opts={}) 
-      miq_domain = opts.fetch(:miq_domain, branch_name.split(/-/)[2]) || branch_name
-      @git_repo  = opts.fetch(:git_repo, nil) || $git_repo
+    def initialize(branch_name, opts={})
       _set_defaults(opts)
-      $logger.debug("Creating Feature: branch=#{branch_name} domain=#{miq_domain}")
+      @name = opts.fetch(:feature_name, branch_name.split(/-/)[2]) || branch_name
+      $logger.debug("Creating Feature: branch=#{branch_name} domain=#{@name}")
 
+      @git_repo  = opts.fetch(:git_repo, nil) || $git_repo
       raise GitFlow::Error, 'Unable to find git repo' if @git_repo.nil?()
       _create_git(branch_name, @git_repo)
-      _create_miq(miq_domain)
+
+      method   = opts.fetch(:miq_import_method, 'partial')
+      provider = opts.fetch(:provider,          'default')
+      @miq_domain  = discover_domains(provider: provider, miq_import_method: method)
     end
 
     # Deploys the feature to ManageIQ
+    # This will deploy a new Automate Domain for every Domain found on the file system, with at 
+    # least one changed file
     #
     def deploy()
       @git_repo.checkout(@git_branch)
-      $logger.info("Deploying: #{@miq_domain}") 
+      meth = @miq_import_method
+      deploy_opts = {:changeset=>get_diff_paths(), :git_workdir=>@git_repo.workdir, :skip_empty=>[:partial].include?(meth), :miq_import_method=>meth}
+      @miq_domain.each do |domain|
+        $logger.info("Deploying: #{domain.name}")
+        domain.deploy(deploy_opts)
+      end 
 
-      import_dir = prepare_import(@miq_import_method, @miq_domain)
-      @miq_provider.import(File.join(import_dir, @automate_dir), @miq_fs_domain, @miq_domain)
-      cleanup_import(@miq_import_method)
-      
+    end
+
+    # Searches for Automate Domain exports on the file system
+    #
+    def discover_domains(opts={})
+      feature_level_params = {:feature_name=>@name, :branch_name => @git_branch.name, :provider=>opts[:provider], :miq_import_method=>opts[:miq_import_method]}
+      domains = find_domain_files(@git_repo.workdir)
+      domains.map{|dom| GitFlow::MiqDomain.create_from_file(dom.merge(feature_level_params))}
+    end
+
+    def show_details()
+      commit = @git_base
+      paths  = get_diff_paths()
+      ret = []
+      ret << "Feature: #{@name} on branch #{@git_branch.name} "
+      ret << " Branch: #{@git_branch.target.tree_id}: #{@git_branch.target.summary}"
+      ret << "   Base: #{commit.tree_id}: #{commit.summary}"
+      ret << ""
+      @miq_domain.each do|dom|
+        ret << dom.name
+        dom._limit_changeset(paths).each{|path| ret << "  #{path}" }
+      end
+      return ret.join("\n")
+    end
+    def show_summary()
+      paths  = get_diff_paths()
+      domain_info = @miq_domain.map{|dom| {:name=>dom.name, :change_num=>dom._limit_changeset(paths).length}}
+      domain_string = domain_info.map{|d| "#{d[:name]}: #{d[:change_num]}"}.join(' ')
+      return "#{@git_branch.name}: #{domain_string}"
     end
 
   end
 end
-

lib/git/mixin_git.rb
@@ -4,7 +4,7 @@ module GitFlow
   # The module where all the git related stuff goes in
   module GitMethods
 
-    # This sets up the git half of a feature
+    # Sets up basic git related stuff
     # 
     # @git_base: The baseline Rugged::Commit to diff against
     # @git_branch: Rugged::Branch of origin/<branch> 
@@ -49,7 +49,7 @@ module GitFlow
     # Get a list of changed files 
     #
     # @param [Rugged::Commit, Rugged::Branch] base defaults to @git_base
-    # @param [Rugged::Commit, Rugged::Branch] base defaults to @git_branch
+    # @param [Rugged::Commit, Rugged::Branch] head defaults to @git_branch
     # @return [Array<String>] A list of changed paths
     def get_diff_paths(base=nil, head=nil)
       base = base || @git_base
@@ -68,6 +68,18 @@ module GitFlow
         paths << delta.new_file[:path]
       end
       paths
+    end
+
+    # Get a list of existing remote branches, matching any prefix
+    #
+    # @param [Array<String>] prefix limit returned list to these prefixes
+    # @param [String] git remote name used to construct branch name. default: origin
+    # @return [Rugged::Branch] all remote pranches starting with prefix
+    def self.get_remote_branches(prefix=nil, remote=nil, repo=nil)
+      prefixes    = prefix || @prefix || ['feature', 'fix', 'master']
+      remote_name = remote || @remote || 'origin'
+      repo        = @git_repo || $git_repo
+      repo.branches.each(:remote).select{|remote_branch| prefixes.any?{|prefix| remote_branch.name.match("#{remote_name}/#{prefix}")  } }
     end
 
   end

lib/gitflow.rb
@@ -2,16 +2,17 @@ module GitFlow
   class Error < StandardError; end  
 
   def self.init()
+    # configure logging
+    #
+    $logger  = Logger.new(STDOUT)
+    $logger.level = $default_opts.fetch(:log_level, Logger::INFO)
+
     # prepare directories
     #
     $tmpdir = Dir.mktmpdir('miq_import_')
     Dir.mkdir(File.join($tmpdir, 'repo'))
     Dir.mkdir(File.join($tmpdir, 'import'))
-
-    # configure logging
-    #
-    $logger  = Logger.new(STDOUT)
-    $logger.level = $default_opts.fetch(:log_level, Logger::INFO)
+    $logger.debug("Using tmp directory: #{$tmpdir}")
 
     # get git repository
     #
@@ -38,7 +39,6 @@ module GitFlow
     $git_path     = ENV['GIT_PATH']     || $git_path
 
     # MIQ params
-    $default_opts[:feature_defaults][:miq_fs_domain] = ENV['EXPORT_NAME'] || ENV['MIQ_FS_DOMAIN'] || $export_name
 
     # Misc
     $default_opts[:log_level] = Logger::DEBUG if ENV['VERBOSE'] == 'true' 

commit 6a29c7b6f2f9b856c702fff969a1a6ccfe236930

fix bug introduced in #17

lib/domain.rb
@@ -42,7 +42,7 @@ module GitFlow
     def self.create_from_file(dom)
       opts = {}
       opts[:export_name]   = dom[:domain_name]
-      opts[:export_dir]    = dom[:relative_path]
+      opts[:export_dir]    = File.dirname(dom[:relative_path])
       opts[:import_method] = dom[:import_method] 
       opts[:provider_name] = dom[:provider]
       opts[:branch_name]   = dom[:branch_name]

lib/miq/method_partial.rb
@@ -19,6 +19,7 @@ module GitFlow
       { :import_dir => import_dir }
     end
     def cleanup_import_partial()
+      {}
     end
 
     module Partial