7f40764c35652baa4b6582a7876bbcf2d760994d
Thomas Buchinger <thomasbuchinger@hotmail.com>
2018-11-24 02:11:23 +0000

    debug travis IV

--- script/verify_import.sh
+++ script/verify_import.sh
@@ -4,7 +4,16 @@ SUITE=${SUITE-default}
 if [[ $SUITE != "integration" ]]; then exit 0; fi
 bundle exec ./bin/cli.rb deploy feature-1-f1 --provider docker
 
+echo "=== Docker PS ======================================================================"
+docker ps
+echo "=== Docker Logs ===================================================================="
+docker logs manageiq
+echo "===================================================================================="
 RESPONSE=$(curl -k --user admin:smartvm "https://localhost:8443/api/automate/${DOMAIN}?depth=-1&attributes=klass,domain_fnname")
+echo "==== Response ======================================================================"
+echo $RESPONSE
+echo "===================================================================================="
+
 
 ERROR=$(echo $RESPONSE | ruby -rjson -e "puts JSON.load(STDIN).dig('error', 'kind')")
 if [[ $ERROR == "bad_request" ]]; then
