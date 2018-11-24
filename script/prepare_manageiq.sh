cab3aab4129c285954f74c01f897226bdcfb54f4
Thomas Buchinger <thomasbuchinger@hotmail.com>
2018-11-24 00:23:42 +0000

    fix travis config

--- script/prepare_manageiq.sh
+++ script/prepare_manageiq.sh
@@ -3,11 +3,12 @@ CONTAINER_NAME=${CONTAINER_NAME-manageiq}
 TAG=${TAG-hammer-1-rc1}
 
 docker rm -f $CONTAINER_NAME
+docker pull manageiq/manageiq:$TAG
 
 docker run --name $CONTAINER_NAME --privileged -d -p 8443:443 manageiq/manageiq:$TAG
 
 sleep 180
-while true
+for i in 1..20
 do
   curl -k --user admin:smartvm -o /dev/null https://localhost:8443/api/servers/1
   CURL_RC=$?
