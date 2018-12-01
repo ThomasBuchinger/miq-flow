af6f2d8bd872c501eae648fc454a5d62d3f5fc43
ThomasBuchinger <ThomasBuchinger@users.noreply.github.com>
2018-12-01 17:59:35 +0100

    implement yaml based config (#26)
    * implement yaml based config
    * add cli flags
    * add default config file
    * add searchpath for config files
    * fix travis
    * improve cleanup. only delete files we used, instead of the whole directory

--- bin/cli.rb
+++ bin/cli.rb
@@ -5,44 +5,63 @@ require_relative '../lib/bootstrap.rb'
 module GitFlow
   # Implements the CLI interface
   class Cli < Thor
+    class_option :verbose, type: :boolean, desc: 'Turn on verbose logging'
+    class_option :quiet, type: :boolean, desc: 'Only show errors and warnings'
+    class_option :cleanup, type: :boolean, desc: 'Clean up the working dir before exiting'
+    class_option :workdir, type: :string, desc: 'Override the working directory'
+    class_option :config, type: :string, alias: '-c', desc: 'Specify config file to load'
+
+    no_commands do
+      def cli_setup
+        GitFlow::Settings.process_config_file(options['config'])
+        GitFlow::Settings.update_log_level(:debug) if options['verbose'] == true
+        GitFlow::Settings.update_log_level(:warn)  if options['quiet'] == true
+        GitFlow::Settings.update_clear_tmp(options['cleanup'])
+        GitFlow::Settings.update_workdir(options['workdir'])
+
+        GitFlow.init()
+      end
+    end
+
     desc 'list', 'List Feature branches'
     def list
-      GitFlow.init()
+      cli_setup()
       branches = GitFlow::GitMethods.get_remote_branches()
       text = branches.map{ |b| GitFlow::Feature.new(b.name, {}).show_summary() }
       puts text
+      GitFlow.tear_down()
     end
 
     desc 'inspect NAME', 'List domains'
     option :short, type: :boolean, default: false
     def inspect(name)
-      GitFlow.init()
+      cli_setup()
       feature = GitFlow::Feature.new(name, {})
       text = options[:short] ? feature.show_summary() : feature.show_details()
       puts text
+      GitFlow.tear_down()
     end
 
-    desc 'deploy NAME', 'Deploy a Feature Branch'
-    option :domain, desc: 'specify the automate domain to use (default: 3rd segment of NAME, seperted by \'-\')'
-    option :export_name, desc: 'name of the domain on the filesystem', alias: 'miq_fs_domain'
+    desc 'deploy BRANCH', 'Deploy a Feature Branch'
+    option :name, desc: 'specify domain identifier (default: 3rd segment of NAME, separated by \'-\')'
     option :priority, type: :numeric
     option :provider, desc: 'How to talk to ManageIQ (default: noop)'
-    def deploy(name)
-      GitFlow.init()
-      miq_domain = options[:domain] || name.split(/-/)[2] || name
+    def deploy(branch)
+      cli_setup()
+      miq_domain = options[:name] || branch.split(/-/)[2] || branch
       provider   = options.fetch(:provider, 'default')
       prio       = options[:miq_priority]
 
       opts = { feature_name: miq_domain, provider: provider }
       opts[:miq_priotiry] = prio
-      feature = GitFlow::Feature.new(name, opts)
+      feature = GitFlow::Feature.new(branch, opts)
       feature.deploy()
       GitFlow.tear_down()
     end
 
     desc 'devel1', 'Development NOOP command'
     def devel1
-      GitFlow.init()
+      cli_setup()
       feature_opts = { feature_name: 'f1' }
       f1 = GitFlow::Feature.new('feature-1-f1', feature_opts)
       f1.deploy()
@@ -51,7 +70,7 @@ module GitFlow
 
     desc 'devel2', 'Development ACTIVE command'
     def devel2
-      GitFlow.init()
+      cli_setup()
       feature_opts = { feature_name: 'base', provider: 'docker', miq_import_method: :clean }
       master = GitFlow::Feature.new('master', feature_opts)
       master.deploy()
--- lib/bootstrap.rb
+++ lib/bootstrap.rb
@@ -1,6 +1,7 @@
 require 'rugged'
 require 'tmpdir'
 require 'logger'
+require 'yaml'
 
 lib_dir = __dir__
 $LOAD_PATH << lib_dir
@@ -10,14 +11,27 @@ require 'miq/provider_noop.rb'
 require 'miq/method_partial.rb'
 require 'miq/method_clean.rb'
 require 'miq/mixin_miq.rb'
+require 'mixin_settings.rb'
 require 'domain.rb'
 require 'feature.rb'
 require 'gitflow.rb'
 
-$default_opts = { clear_tmp: true }
-$default_opts[:miq] = {}
-$default_opts[:git_opts] = {}
-require_relative '../custom.rb' if File.file?(File.expand_path(File.join(lib_dir, '..', 'custom.rb')))
-GitFlow.process_environment_variables()
-$default_opts.freeze()
+$settings = {}
+$settings[:miq] = {}
+$settings[:git] = {}
+GitFlow::Settings.set_defaults()
+SEARCHPATH = [
+  'config.yml',
+  'config.yaml',
+  'gitflow.yml',
+  'gitflow.yaml',
+  File.expand_path('~/.gitflow.yml'),
+  File.expand_path('~/.gitflow.yaml'),
+  File.expand_path('~/.gitflow/config.yml'),
+  File.expand_path('~/.gitflow/config.yaml')
+].freeze
+SEARCHPATH.each do |file|
+  GitFlow::Settings.process_config_file(file)
+end
+GitFlow::Settings.process_environment_variables()
 GitFlow.validate() ? true : exit(1)
